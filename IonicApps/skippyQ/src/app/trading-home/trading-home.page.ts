1fa3543b50da6b72f5c995237e23d690fd61d8af
bryanshingg <bryanshing21@gmail.com>
2023-09-02 18:15:33 +0800

    Fixed up trading home page clicks

--- IonicApps/skippyQ/src/app/trading-home/trading-home.page.ts
+++ IonicApps/skippyQ/src/app/trading-home/trading-home.page.ts
@@ -9,14 +9,14 @@ import { collection, getDocs } from "firebase/firestore";
 import { trading } from '../shared/models/trading';
 import { InterceptorService } from '../shared/services/interceptor.service';
 import { Storage } from '@ionic/storage';
-import { HttpClient,HttpRequest,HttpHandler,HttpEvent,HttpHeaders} from '@angular/common/http';
-import { from, Observable,catchError, } from 'rxjs';
-import {SqlService} from '../shared/services/sqldb.service'
+import { HttpClient, HttpRequest, HttpHandler, HttpEvent, HttpHeaders } from '@angular/common/http';
+import { from, Observable, catchError, } from 'rxjs';
+import { SqlService } from '../shared/services/sqldb.service'
 
 
 
 interface HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler):   Observable<HttpEvent<any>>
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>
 }
 
 @Component({
@@ -24,203 +24,139 @@ interface HttpInterceptor {
   templateUrl: 'trading-home.page.html',
   styleUrls: ['trading-home.page.scss'],
 })
-export class TradingHomePage implements OnInit{
+export class TradingHomePage implements OnInit {
 
-clicks = parseInt(localStorage.getItem("pageClicks"))
+  clicks = parseInt(localStorage.getItem("pageClicks"))
 
+  startTime!: number
+  initTime!: number
+  contentInitTime!: number
+  viewInitTime!: number
+  dbStartTime!: number
 
-
-startTime! : number
-initTime! : number
-contentInitTime! : number
-viewInitTime! : number
-dbStartTime! : number
-
-
-
- orderStatus="";
- item = [];
- orderType= "";
- portfolioType= "";
- portfolioId = "";
- units="";
- validity="";
- backEndErrors = 0;
+  orderStatus = "";
+  item = [];
+  orderType = "";
+  portfolioType = "";
+  portfolioId = "";
+  units = "";
+  validity = "";
+  backEndErrors = 0;
 
 
 
- constructor(public http: HttpClient,public router: Router,private trackingService: TrackingService, private SqlService: SqlService){
-  this.startTime = window.performance.now()
-  localStorage.setItem("startTime", JSON.stringify(this.startTime))
-  console.log(this.startTime/1000)
-
-
- }
- 
-
- details=new Array<any>();;
- trading=new Array<any>();
- selectedArray=new Array<any>();
-
-
-
- ngOnInit() {
-
-  
-
-  this.initTime = window.performance.now()
-  
-  
-
-  
-  
-
-  this.SqlService.getTrading().subscribe(result => {
-
-    
-   
-    var jsonbody = result['Data']['recordset']
-    
-    for (var i = 0; i < jsonbody.length; i++){
-    var newTrade = new trading(jsonbody[i]['foreignBrokerage'],jsonbody[i]['marketValue'],jsonbody[i]['orderNumber'],
-    jsonbody[i]['orderStatus'],jsonbody[i]['orderType'],jsonbody[i]['portfolioId'],jsonbody[i]['portfolioType'],jsonbody[i]['priceLimit'],
-    jsonbody[i]['quantity'],jsonbody[i]['settlementCurrency'],jsonbody[i]['tradingPlace'],jsonbody[i]['triggerLimit'],
-    jsonbody[i]['units'],jsonbody[i]['validity'],jsonbody[i]['enteredOn'],jsonbody[i]['executedStocks'])
-    // this.selectedArray.push(newTrade)
-    this.details.push(newTrade)
-    this.pending()
-    this.orderStatus = "pending"
-    
-
+  constructor(public http: HttpClient, public router: Router, private trackingService: TrackingService, private SqlService: SqlService) {
+    this.startTime = window.performance.now()
+    localStorage.setItem("startTime", JSON.stringify(this.startTime))
+    console.log(this.startTime / 1000)
   }
 
-  }, error => {
-    this.backEndErrors += 1
-    localStorage.setItem("backEndErrors", JSON.stringify(this.backEndErrors))
-    console.log(error)
-    
-  })
 
-  localStorage.setItem("pageLoadTime", JSON.stringify((this.initTime-this.startTime)/1000))
-  localStorage.setItem("dbLoadTime",JSON.stringify(window.performance.now()-parseFloat(localStorage.getItem("dbLoadTime"))))
-
-  
-
-}
-
-  
-   
-  
- 
+  details = new Array<any>();;
+  trading = new Array<any>();
+  selectedArray = new Array<any>();
 
 
 
+  ngOnInit() {
+
+    this.initTime = window.performance.now()
+    this.orderStatus = "pending"
 
 
+    this.SqlService.getTrading().subscribe(result => {
+
+      var jsonbody = result['Data']['recordset']
+
+      for (var i = 0; i < jsonbody.length; i++) {
+        var newTrade = new trading(jsonbody[i]['foreignBrokerage'], jsonbody[i]['marketValue'], jsonbody[i]['orderNumber'],
+          jsonbody[i]['orderStatus'], jsonbody[i]['orderType'], jsonbody[i]['portfolioId'], jsonbody[i]['portfolioType'], jsonbody[i]['priceLimit'],
+          jsonbody[i]['quantity'], jsonbody[i]['settlementCurrency'], jsonbody[i]['tradingPlace'], jsonbody[i]['triggerLimit'],
+          jsonbody[i]['units'], jsonbody[i]['validity'], jsonbody[i]['enteredOn'], jsonbody[i]['executedStocks'])
+        this.details.push(newTrade)
+      }
+
+      this.pending(false)
+
+    }, error => {
+      this.backEndErrors += 1
+      localStorage.setItem("backEndErrors", JSON.stringify(this.backEndErrors))
+    })
+
+    localStorage.setItem("pageLoadTime", JSON.stringify((this.initTime - this.startTime) / 1000))
+    localStorage.setItem("dbLoadTime", JSON.stringify(window.performance.now() - parseFloat(localStorage.getItem("dbLoadTime"))))
+  }
 
 
-pending(){
+  pending(count = true) {
+    this.selectedArray = this.details.filter(detail => detail.orderStatus === "Pending")
+    if (count == true) {
+      this.clicks += 1
+      localStorage.setItem("pageClicks", JSON.stringify(this.clicks))
+      console.log("In pending: ", this.clicks)
+    }
+  }
+
+  executed() {
+    this.selectedArray = this.details.filter(detail => detail.orderStatus == "Executed" || detail.orderStatus == "Partially Executed")
+    this.clicks += 1
+    localStorage.setItem("pageClicks", JSON.stringify(this.clicks))
+    console.log("In executed: ", this.clicks)
+  }
 
 
-  this.selectedArray = this.details.filter(detail => detail.orderStatus === "Pending")
-  console.log(this.selectedArray)
+  closed() {
+    this.selectedArray = this.details.filter(detail => detail.orderStatus === "Closed")
+    this.clicks += 1
+    localStorage.setItem("pageClicks", JSON.stringify(this.clicks))
+    console.log("In closed: ", this.clicks)
+  }
 
-  this.clicks +=1
+  viewAll() {
 
-  localStorage.setItem("pageClicks",JSON.stringify(this.clicks))
+    this.selectedArray = this.details
+    this.clicks += 1
 
+    localStorage.setItem("pageClicks", JSON.stringify(this.clicks))
+  }
 
-
- }
-
- executed(){
- this.selectedArray = this.details.filter(detail => detail.orderStatus == "Executed" || detail.orderStatus == "Partially Executed"  )
- this.clicks += 1
- localStorage.setItem("pageClicks",JSON.stringify(this.clicks))
-
-
- }
-
-
- closed(){
-
- this.selectedArray = this.details.filter(detail => detail.orderStatus === "Closed")
- this.clicks +=1
- localStorage.setItem("pageClicks",JSON.stringify(this.clicks))
+  buyRoute() {
+    this.clicks += 1
+    localStorage.setItem("pageClicks", JSON.stringify(this.clicks))
+  }
 
 
- }
-
-viewAll(){
-
- this.selectedArray = this.details
- this.clicks +=1
-
- localStorage.setItem("pageClicks",JSON.stringify(this.clicks))
+  sellRoute() {
+    this.clicks += 1
+    localStorage.setItem("pageClicks", JSON.stringify(this.clicks))
+  }
 
 
+  modify(index) {
 
- }
+    this.clicks += 1
 
- buyRoute(){
-  this.clicks +=1
-  localStorage.setItem("pageClicks",JSON.stringify(this.clicks))
+    localStorage.setItem("pageClicks", JSON.stringify(this.clicks))
 
-
- }
-
-
- sellRoute(){
-  this.clicks +=1
-  localStorage.setItem("pageClicks",JSON.stringify(this.clicks))
-
-
- }
-
-
- modify(index){
-
-
-
-  this.clicks +=1
-
-  localStorage.setItem("pageClicks",JSON.stringify(this.clicks))
-
-
-
-
-
-
-
-
-      this.router.navigate(['/trade-details',
+    this.router.navigate(['/trade-details',
       {
-      enteredOn:this.selectedArray[index].enteredOn,
-      quantity: this.selectedArray[index].quantity,
-      settlementCurrency: this.selectedArray[index].settlementCurrency,
-      foreignBrokerage: this.selectedArray[index].foreignBrokerage,
-      executedStocks:this.selectedArray[index].executedStocks,
-      orderType:this.selectedArray[index].orderType,
-      portfolioType:this.selectedArray[index].portfolioType,
-      portfolioId:this.selectedArray[index].portfolioId,
-      orderStatus:this.selectedArray[index].orderStatus,
-      units:this.selectedArray[index].units,
-      validity:this.selectedArray[index].validity,
-      marketValue:this.selectedArray[index].marketValue,
-      triggerLimit:this.selectedArray[index].triggerLimit,
-      priceLimit:this.selectedArray[index].priceLimit
+        enteredOn: this.selectedArray[index].enteredOn,
+        quantity: this.selectedArray[index].quantity,
+        settlementCurrency: this.selectedArray[index].settlementCurrency,
+        foreignBrokerage: this.selectedArray[index].foreignBrokerage,
+        executedStocks: this.selectedArray[index].executedStocks,
+        orderType: this.selectedArray[index].orderType,
+        portfolioType: this.selectedArray[index].portfolioType,
+        portfolioId: this.selectedArray[index].portfolioId,
+        orderStatus: this.selectedArray[index].orderStatus,
+        units: this.selectedArray[index].units,
+        validity: this.selectedArray[index].validity,
+        marketValue: this.selectedArray[index].marketValue,
+        triggerLimit: this.selectedArray[index].triggerLimit,
+        priceLimit: this.selectedArray[index].priceLimit
 
-       }
-
-          ])
-
-
-
- }
-
-
-
-
-
+      }
+    ])
+  }
 
 }
