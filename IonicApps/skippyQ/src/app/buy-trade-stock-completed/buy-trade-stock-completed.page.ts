0c2018154b5348833eb1a8b0f7d1fdabfbb4d7a1
bryanshingg <bryanshing21@gmail.com>
2023-09-10 21:38:29 +0800

    completed 2 user stories metrics under placeing market order or limit order

--- IonicApps/skippyQ/src/app/buy-trade-stock-completed/buy-trade-stock-completed.page.ts
+++ IonicApps/skippyQ/src/app/buy-trade-stock-completed/buy-trade-stock-completed.page.ts
@@ -1,77 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 // import {DataService} from '../services/data.service';
 import { Router, ActivatedRoute } from '@angular/router';
-
-
+import { TrackingService } from './../shared/services/tracking.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: 'buy-trade-stock-completed.page.html',
   styleUrls: ['buy-trade-stock-completed.page.scss']
 })
-export class BuyTradeStockCompletedPage implements OnInit{
+export class BuyTradeStockCompletedPage implements OnInit {
 
-today = new Date();
+  today = new Date();
 
-startTime! : number;
-initTime! : number;
-contentInitTime! : number;
-viewInitTime! : number;
+  clicks = 0
 
+  startTime!: number
+  initTime!: number
+  dbStartTime!: number
+  contentInitTime!: number
+  viewInitTime!: number
+  dbloadTime!: number
+  backEndErrors = 0;
+  pageName: string = "buyTradeStockCompleted";
 
-clicks = parseInt(localStorage.getItem('pageClicks'))
-
-
-
-
-
-  constructor(public route: ActivatedRoute, public router: Router ){
+  constructor(public route: ActivatedRoute, public router: Router, private trackingService: TrackingService) {
     this.startTime = window.performance.now()
     localStorage.setItem("startTime", JSON.stringify(this.startTime))
-    console.log(this.startTime/1000)
   }
-
-
-//   constructor(private dataService: DataService) {
-//   }
-
-
-
   ngOnInit() {
     this.initTime = window.performance.now()
-    localStorage.setItem("pageLoadTime", JSON.stringify((this.initTime-this.startTime)/1000))
-  
+    localStorage.setItem("pageLoadTime", JSON.stringify((this.initTime - this.startTime)))
 
+    if (JSON.parse(localStorage.getItem("userStoryName")) == "Successfully place market order " && JSON.parse(localStorage.getItem("selectOrder")) == "Market Order") {
+      setTimeout(() => {
+        alert('You have completed this user story!');
+        localStorage.clear();
+        this.trackingService.trackJourneyMetrics(window.performance.now());
+        location.reload();
+        this.router.navigate(['/user-stories'])
+      }, 200);
+    }
+    if (JSON.parse(localStorage.getItem("userStoryName")) == "Successfully places a limit market order " && JSON.parse(localStorage.getItem("selectOrder")) == "Limit Order") {
+      setTimeout(() => {
+        alert('You have completed this user story!');
+        localStorage.clear();
+        this.trackingService.trackJourneyMetrics(window.performance.now());
+        location.reload();
+        this.router.navigate(['/user-stories'])
+      }, 200);
+    }
   }
-  newOrder(){
-  this.router.navigate(['/buy-trade'])
-  this.clicks +=1
-  localStorage.setItem('pageClicks', JSON.stringify(this.clicks))
 
-  
+  crossButton() {
+    this.clicks += 1
+    localStorage.setItem("pageClicks", JSON.stringify(this.clicks))
+    this.trackingService.trackCTAMetrics(this.pageName, "button", "click on cross button", "tradingHome", 0);
   }
 
-
-
-
-
-
-
-
-
-
-//   async loadData(){
-//   this.dataService.getData().subscribe(res => {
-//   this.listData = res
-//   }
-//   );
-//   }
-
-
-
-
-
-
-   }
+  newOrder() {
+    this.clicks += 1
+    localStorage.setItem('pageClicks', JSON.stringify(this.clicks))
+    this.trackingService.trackCTAMetrics(this.pageName, "button", "click on new order button", "buyTrade", 0);
+    this.router.navigate(['/buy-trade'])
+  }
+}
 
 
